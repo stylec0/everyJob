28912958216f09652553ec69efcecd6d7f38a739
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-01-28 15:16:18 -0800

    Test for trisha

--- main_app/models.py
+++ main_app/models.py
@@ -2,27 +2,31 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
+# To be edited/added later
 INDUSTRIES = (
-        ('H', 'Healthcare'),
-        ('IT', 'Information Technology'),
-        ('RE', 'Real Estate and Development'),
-        ('R', 'Retail'),
-        ('EDU', 'Education'),
-        ('G', 'Government'),
-        ('E', 'Engineering'),
-        ('D', 'Design'),
-        ('A', 'Art'),
-        ('S', 'Science'),
+    ('H', 'Healthcare'),
+    ('IT', 'Information Technology'),
+    ('RE', 'Real Estate and Development'),
+    ('R', 'Retail'),
+    ('EDU', 'Education'),
+    ('G', 'Government'),
+    ('E', 'Engineering'),
+    ('D', 'Design'),
+    ('A', 'Art'),
+    ('S', 'Science'),
 )
+
 
 class JobTitle(models.Model):
     job_title = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.job_title
 
-    #def get_absolute_url(self):
+    # def get_absolute_url(self):
     #    return reverse('title_detail', kwargs={'pk': self.id})
+
 
 class JobPost(models.Model):
     job_title = models.ManyToManyField(JobTitle)
@@ -37,5 +41,5 @@ class JobPost(models.Model):
     def __str__(self):
         return self.job_title
 
-    #def get_absolute_url(self):
-    #    return reverse('post_detail', kwargs={'pk': self.id}) 
+    # def get_absolute_url(self):
+    #    return reverse('post_detail', kwargs={'pk': self.id})
