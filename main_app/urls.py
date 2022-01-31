dde950be41c33a2b286a64cde94f6d0bac3efce1
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-01-30 16:55:42 -0800

    Add basic authentication, edit views.py, urls.py, templates, registered the Admin app

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,8 @@ from . import views  # views is the name of the file
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
     # path('about/', views.about, name='about'),
+    path('everyjob/', views.job_detail, name='detail'),
     # We will have login, logout, jobtitle/index/home, jobpost detail routes
+    path('accounts/signup/', views.signup, name='signup'),
 ]
