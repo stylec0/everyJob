80a82f710131f7fac606eee2095205fffbb21597
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-01-31 10:28:02 -0800

    Add job title functionality merge with group work

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,10 @@ from . import views  # views is the name of the file
 urlpatterns = [
     path('', views.home, name='home'),
     # We will have login, logout, jobtitle/index/home, jobpost detail routes
+
+
+    path('everyjob/create/', views.JobTitleCreate.as_view(),
+         name='job_title_create'),
     path('accounts/signup/', views.signup, name='signup'),
     path('everyjob/<int:job_title_id>/add_job_post/',
          views.JobPostCreate.as_view(), name='add_job_post'),
