19b8658f7af7bc73563637aecedb90808a0e5711
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-01-31 16:07:35 -0800

    MVP

---
+++ main_app/forms.py
@@ -0,0 +1,9 @@
+
+from django.forms import ModelForm
+from .models import JobPost
+
+
+class JobPostForm(ModelForm):
+    class Meta:
+        model = JobPost
+        fields = ['industry', 'details', 'years_experience']
--- main_app/urls.py
+++ main_app/urls.py
@@ -12,12 +12,13 @@ urlpatterns = [
          name='job_title_create'),
     path('accounts/signup/', views.signup, name='signup'),
     path('everyjob/<int:job_title_id>/add_job_post/',
-         views.JobPostCreate.as_view(), name='add_job_post'),
+         views.GetJobPostForm, name='add_job_post'),
+    path('everyjob/<int:job_title_id>/post_job/',
+         views.JobPostCreate, name='post_job'),
     # path('everyjob/<int:job_title_id>/',
     #      views.JobPostCreate, name='add_job_post'),
     path('everyjob/create/', views.JobTitleCreate.as_view(),
          name='job_title_create'),
     path('everyjob/<int:job_title_id>/',
          views.job_title_detail, name='detail'),
-
 ]
