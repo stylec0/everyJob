542ad1be3ede56405c550c70485a26fceb85027c
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2022-02-01 11:51:36 -0800

    Working on Edit function

--- main_app/views.py
+++ main_app/views.py
@@ -63,19 +63,20 @@ def GetJobPostUpdate(request, job_title_id):
     })
 
 @login_required
-def UpdateJobPost(request, job_title_id):
+def UpdateJobPost(request, job_title_id, job_post_id):
     title = JobTitle.objects.filter(pk=job_title_id)
     # job_post value needs to be the job post we are trying to edit specifically
-    job_post = JobPost.objects.filter(job_title=job_title_id)
+    job_post = JobPost.objects.filter(id=job_post_id)
     job_form = JobPostUpdateForm(request.POST)
     print(job_post, "<---this is the job_post")
+    #print(job_post_id, "<---this is the job_post_id")
     if job_form.is_valid():
         update_job_post = job_form.save(commit=False)
         update_job_post.user_id = request.user.id
         # save every time new_job_post gets manipulated
-        job_post.update(update_job_post)
+        update_job_post.save()
         update_job_post.job_title.set(title)
-        job_post.update()
+        update_job_post.save()
         # return to detail of selected Job Title
         # ('detail' path in urls.py)
         # this first job_title_id is taking the parameter
