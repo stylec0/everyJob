9d8cc5d43f064f52e25378e264cf8d24b295e0e3
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-02-01 09:39:40 -0800

    Add notes and delete non-functional code

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,18 +5,15 @@ from . import views  # views is the name of the file
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # We will have login, logout, jobtitle/index/home, jobpost detail routes
-
-
     path('everyjob/create/', views.JobTitleCreate.as_view(),
          name='job_title_create'),
     path('accounts/signup/', views.signup, name='signup'),
+    # on JobTitle detail, when you click add JobPost
     path('everyjob/<int:job_title_id>/add_job_post/',
          views.GetJobPostForm, name='add_job_post'),
+    # on AddJobPost Form page, when you click submit
     path('everyjob/<int:job_title_id>/post_job/',
          views.JobPostCreate, name='post_job'),
-    # path('everyjob/<int:job_title_id>/',
-    #      views.JobPostCreate, name='add_job_post'),
     path('everyjob/create/', views.JobTitleCreate.as_view(),
          name='job_title_create'),
     path('everyjob/<int:job_title_id>/',
